eaf6117a74eedcf29b3663344d5939323b29806f
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-06-18 12:15:26 -0700

    added score component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import Card from './Card';
+import Score from './Score';
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    // Pick a remaining element
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
 
 export function App() {
+  const [gameOver, setGameOver] = useState(false);
   return (
-    <div className='board'>
-      <Card name='Chowder' src='../public/Chowder.jpeg'></Card>
-      <Card name='Mung Daal' src='../public/MungDaal.jpeg'></Card>
+    <div>
+      <Score></Score>
+      <div className='board'>
+        <Card name='Chowder' src='Chowder.jpeg'></Card>
+        <Card name='Mung Daal' src='MungDaal.jpeg'></Card>
+      </div>
     </div>
   );
 }
---
+++ src/components/Score.jsx
@@ -0,0 +1,10 @@
+function Score() {
+  return (
+    <div className='score'>
+      <p> Score: 0</p>
+      <p>High Score:</p>
+    </div>
+  );
+}
+
+export default Score;
